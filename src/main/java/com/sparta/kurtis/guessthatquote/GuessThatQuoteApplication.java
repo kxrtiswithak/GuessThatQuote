05309afb6b7d62ea4f99ab82e2ce78517fb75af3
kxrtiswithak <kxrtiswithak@gmail.com>
2021-03-17 16:43:00 +0000

    initial commit to dev branch

---
+++ src/main/java/com/sparta/kurtis/guessthatquote/GuessThatQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kurtis.guessthatquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessThatQuoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessThatQuoteApplication.class, args);
+    }
+
+}
